7e8e641e538b3b8f900d3c842e7fcdc460d0d74e
thalmannpioneer <82112296+thalmannpioneer@users.noreply.github.com>
2026-04-19 17:20:44 +0300

    jy6j56j567j567

--- app.js
+++ app.js
@@ -11,11 +11,10 @@ const util = require('util');
 const server = express();
 const PORT = 80;
 
-fs.mkdirSync("./uploads");
-
 const storage = {
     async _handleFile(req, file, cb) {
         const hash = crypto.createHash('sha256');
+        fs.mkdirSync("./uploads");
         const stream = fs.createWriteStream('./uploads/temp');
         let finalPath;
 
